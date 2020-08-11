192733092adb07f8130e0353d26931eabf624101
sotela88 <j-ms-27@hotmail.com>
2020-08-11 12:33:40 +0200

    Updates

---
+++ precache-manifest.64367b765107fffd533dbdc44ee9a488.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9846ea9f4729f49342a2b941d38e7a34",
+    "url": "/Mauricio-Sotela/Portfolio/index.html"
+  },
+  {
+    "revision": "336b7006720a66e92975",
+    "url": "/Mauricio-Sotela/Portfolio/static/css/2.cc5754b7.chunk.css"
+  },
+  {
+    "revision": "1fe92ffa2d1be26f70a1",
+    "url": "/Mauricio-Sotela/Portfolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "336b7006720a66e92975",
+    "url": "/Mauricio-Sotela/Portfolio/static/js/2.ab348626.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Mauricio-Sotela/Portfolio/static/js/2.ab348626.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fe92ffa2d1be26f70a1",
+    "url": "/Mauricio-Sotela/Portfolio/static/js/main.abe8c484.chunk.js"
+  },
+  {
+    "revision": "842dc79c023e8d07382a",
+    "url": "/Mauricio-Sotela/Portfolio/static/js/runtime-main.a87879cc.js"
+  }
+]);
