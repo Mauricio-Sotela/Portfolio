7f543e8dc6e8adac89b67eed7aa6c28252cf7785
sotela88 <j-ms-27@hotmail.com>
2020-08-27 09:26:12 +0200

    bg video radio fixed

--- src/App.js
+++ src/App.js
@@ -1,38 +1,20 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-} from "react-router-dom";
-import Home from './components/Home'
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Home from "./components/Home";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
 
 export default function App() {
-
   return (
     <Router>
       <div>
-       <nav className='container main_nav'>
-          <ul>
-            <li>
-              <Link to="/Portfolio">Home</Link>
-            </li>
-            <li>
-              <Link  to="/contact">Contact Me</Link>
-            </li>
-            <li>
-              <Link to="/topics">Topics</Link>
-            </li>
-          </ul>
-       </nav>
-
+        < NavBar />
         <Switch>
           <Route exact path="/contact">
             <Contact />
           </Route>
-          
+
           <Route exact path="/Portfolio">
             <Home />
           </Route>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,23 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
 function NavBar() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div className="nav__container">
+      <nav className="container main_nav">
+        <ul>
+          <li>
+            <Link to="/Portfolio">Home</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Me</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
