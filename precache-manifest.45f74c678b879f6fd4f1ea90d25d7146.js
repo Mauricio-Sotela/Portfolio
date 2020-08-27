4183cedd18fa07989547f730d14c69239ab83a4d
sotela88 <j-ms-27@hotmail.com>
2020-08-27 13:48:10 +0200

    Updates

--- precache-manifest.45f74c678b879f6fd4f1ea90d25d7146.js
+++ precache-manifest.45f74c678b879f6fd4f1ea90d25d7146.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "58006a8bdc21a977912944771f12a395",
+    "revision": "d0a64c766807966b5e5b3a9235fe56ea",
     "url": "/Portfolio/index.html"
   },
   {
-    "revision": "21e1e66f4862a4bd3077",
+    "revision": "e969f266ec4dddd062de",
     "url": "/Portfolio/static/css/2.cc5754b7.chunk.css"
   },
   {
-    "revision": "6ccbca6b68565b4c24c0",
-    "url": "/Portfolio/static/css/main.7f1e5158.chunk.css"
+    "revision": "b9c98bb49475c2d503e3",
+    "url": "/Portfolio/static/css/main.c4b96caf.chunk.css"
   },
   {
-    "revision": "21e1e66f4862a4bd3077",
-    "url": "/Portfolio/static/js/2.018ef950.chunk.js"
+    "revision": "e969f266ec4dddd062de",
+    "url": "/Portfolio/static/js/2.99110a66.chunk.js"
   },
   {
     "revision": "63f309877853efb81650b071b20ca45c",
-    "url": "/Portfolio/static/js/2.018ef950.chunk.js.LICENSE.txt"
+    "url": "/Portfolio/static/js/2.99110a66.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6ccbca6b68565b4c24c0",
-    "url": "/Portfolio/static/js/main.1b84af56.chunk.js"
+    "revision": "b9c98bb49475c2d503e3",
+    "url": "/Portfolio/static/js/main.4ec1c7e5.chunk.js"
   },
   {
     "revision": "4091982705c13033375d",
