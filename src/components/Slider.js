6c74fa3417df4ef338a17219534b5db0bf7657da
sotela88 <j-ms-27@hotmail.com>
2020-09-03 14:42:27 +0200

    carousel links

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -4,58 +4,19 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Data from "../data.json";
 function Slider() {
   let projects = Data.projects.map((project) => {
-   return (<div>
-      <img alt="" src={project.url} />
-      <p className="legend">Legend 1</p>
-    </div>)
+    return (
+      <a href={project.url}>
+        <div>
+          <img alt="" src={project.img} />
+          <p className="legend">Legend 1</p>
+        </div>
+      </a>
+    );
   });
 
   return (
     <Carousel autoPlay infiniteLoop>
       {projects}
-      {/* <div>
-        <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-1.jpg" />
-        <p className="legend">Legend 1</p>
-      </div>
-      <div>
-        <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-2.jpg" />
-        <p className="legend">Legend 2</p>
-      </div>
-      <div>
-        <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-3.jpg" />
-        <p className="legend">Legend 3</p>
-      </div>
-      <div>
-        <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-4.jpg" />
-        <p className="legend">Legend 4</p>
-      </div>
-      <div>
-        <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-5.jpg" />
-        <p className="legend">Legend 5</p>
-      </div>
-      <div>
-        <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-6.jpg" />
-        <p className="legend">Legend 6</p>
-      </div>
-      <div>
-        <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-7.jpg" />
-        <p className="legend">Legend 7</p>
-      </div>
-      <div>
-        <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-8.jpg" />
-        <p className="legend">Legend 8</p>
-      </div>
-      <div>
-        <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-9.jpg" />
-        <p className="legend">Legend 9</p>
-      </div>
-      <div>
-        <img
-          alt=""
-          src="http://lorempixel.com/output/cats-q-c-640-480-10.jpg"
-        />
-        <p className="legend">Legend 10</p>
-      </div> */}
     </Carousel>
   );
 }
